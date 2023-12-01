3ba89c11517704ddbccd3fe52a581945661e1475
adam-ghafara <adam.ghafara.job@gmail.com>
2023-12-01 08:56:36 +0700

    New articlePost

--- articlePost/function.go
+++ articlePost/function.go
@@ -9,15 +9,15 @@ import (
 )
 
 func init() {
-	functions.HTTP("baSignUp", HelloSignUp)
+	functions.HTTP("HelloPostArticle", HelloPostArticle)
 }
 
-func HelloSignUp(w http.ResponseWriter, r *http.Request) {
+func HelloPostArticle(w http.ResponseWriter, r *http.Request) {
 	// Set header Access-Control-Allow-Origin untuk mengizinkan permintaan dari domain yang spesifik.
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	// Set header Access-Control-Allow-Methods untuk mengizinkan metode HTTP yang diizinkan.
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
 
 	// Set header Access-Control-Allow-Headers untuk mengizinkan header yang diizinkan dalam permintaan.
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
@@ -31,8 +31,4 @@ func HelloSignUp(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	response := berkatbepkg.GCFPostArticle("MONGOSTRING", "berkatauto", "articleSet", r)
 	fmt.Fprintf(w, response)
-}
-
-func GetToken(r *http.Request) string {
-	return r.Header.Get("Authorization")
 }
