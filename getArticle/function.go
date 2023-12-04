e3a2cacc438eabe104c154a33425ff2ab3ace961
adam-ghafara <adam.ghafara.job@gmail.com>
2023-12-04 11:42:21 +0700

    BARU

--- getArticle/function.go
+++ getArticle/function.go
@@ -29,6 +29,10 @@ func HelloNowLoading(w http.ResponseWriter, r *http.Request) {
 
 	// Tulis respons Anda ke Writer seperti yang Anda lakukan sebelumnya.
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	response := berkatbepkg.GCFDeleteArticle("MONGOSTRING", "berkatauto", "articleSet", r)
+	response := berkatbepkg.GCFGetArticle("MONGOSTRING", "berkatauto", "articleSet", r)
 	fmt.Fprintf(w, response)
+}
+
+func GetToken(r *http.Request) string {
+	return r.Header.Get("Authorization")
 }
